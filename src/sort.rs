e0e1e1cf1ab11c76f2baea0f8ba780ebadf14230
Alexey <alexey.ernest@gmail.com>
2020-10-20 18:27:00 -0700

    quick sort

--- src/sort.rs
+++ src/sort.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 // Selection sort: run time O(n^2), space - O(1)
 pub fn selection<T>(list: &mut [T]) where T: std::cmp::PartialOrd {
     for i in 0..list.len() {
@@ -108,6 +110,40 @@ fn merge_internal<T>(list: &mut [T], l: usize, mid: usize, r: usize) where T: st
         j += 1;
         k += 1;
     }
+}
+
+// Quick sort: average run time O(n*logn), space - O(1)
+pub fn quick<T>(list: &mut [T]) where T: std::cmp::PartialOrd {
+    quick_internal(list, 0, list.len()-1);
+}
+
+fn quick_internal<T>(list: &mut [T], lo: usize, hi: usize) where T: std::cmp::PartialOrd {
+    if lo >= hi {
+        return;
+    }
+
+    let p = quick_partition(list, lo, hi);
+    if p > 0 {
+        quick_internal(list, lo, p-1);    
+    }
+    quick_internal(list, p+1, hi);
+}
+
+fn quick_partition<T>(list: &mut [T], lo: usize, hi: usize) -> usize 
+    where T: std::cmp::PartialOrd {
+    let mut rng = rand::thread_rng();
+    let pivot_i = lo + rng.gen_range(0, hi-lo+1);
+    list.swap(pivot_i, hi);
+    
+    let mut i = lo;
+    for j in lo..hi {
+        if list[j] < list[hi] {
+            list.swap(i, j);
+            i += 1;
+        }
+    }
+    list.swap(i, hi);
+    return i;
 }
 
 
@@ -264,6 +300,35 @@ mod tests {
     fn merge_desc_sort() {
         let mut input = vec![5, 4, 3, 2, 1];
         merge(&mut input);
+        check_order(&input);
+    }
+
+
+    #[test]
+    fn quick_two_elements() {
+        let mut input = vec![2, 1];
+        quick(&mut input);
+        check_order(&input);
+    }
+
+    #[test]
+    fn quick_basic_sort() {
+        let mut input = vec![1, 3, 2, 5, 4];
+        quick(&mut input);
+        check_order(&input);
+    }
+
+    #[test]
+    fn quick_asc_sort() {
+        let mut input = vec![1, 2, 3, 4, 5];
+        quick(&mut input);
+        check_order(&input);
+    }
+
+    #[test]
+    fn quick_desc_sort() {
+        let mut input = vec![5, 4, 3, 2, 1];
+        quick(&mut input);
         check_order(&input);
     }
 }
