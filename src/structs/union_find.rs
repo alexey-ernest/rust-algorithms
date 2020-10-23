b16226c79509e085469ee2a013ab02e48a5431b3
Alexey <alexey.ernest@gmail.com>
2020-10-22 17:45:25 -0700

    Weighte quick-union implementation

--- src/structs/union_find.rs
+++ src/structs/union_find.rs
@@ -1,5 +1,7 @@
+// Weighted quick-union implementation
 pub struct UnionFind {
     data: Vec<usize>,
+    size: Vec<usize>,
     count: usize,
 }
 
@@ -14,24 +16,40 @@ impl UnionFind {
 
         UnionFind {
             data: data,
+            size: vec![1; size],
             count: size,
-        }
-    }
-
-    pub fn union(&mut self, p: usize, q: usize) {
-        assert!(p < self.data.len());
-        assert!(q < self.data.len());
-
-        if self.data[p] != self.data[q] {
-            self.data[p] = self.data[q];
-            self.count -= 1;    
         }
     }
 
     pub fn find(&self, p: usize) -> usize {
         assert!(p < self.data.len());
 
-        self.data[p]
+        let mut p = p;
+        while p != self.data[p] {
+            p = self.data[p];
+        }
+        p
+    }
+
+    pub fn union(&mut self, p: usize, q: usize) {
+        assert!(p < self.data.len());
+        assert!(q < self.data.len());
+
+        let i = self.find(p);
+        let j = self.find(q);
+        if i == j {
+            return;
+        }
+
+        if self.size[i] < self.size[j] {
+            self.data[i] = j;
+            self.size[j] += self.size[i];
+        } else {
+            self.data[j] = i;
+            self.size[i] += self.size[j];
+        }
+        
+        self.count -= 1;    
     }
 
     pub fn count(&self) -> usize {
@@ -42,7 +60,7 @@ impl UnionFind {
         assert!(p < self.data.len());
         assert!(q < self.data.len());
 
-        self.data[p] == self.data[q]
+        self.find(p) == self.find(q)
     }
 }
 
@@ -68,5 +86,34 @@ mod tests {
         uf.union(0, 1);
         assert_eq!(uf.count(), 1);
         assert_eq!(uf.connected(0, 1), true);
+    }
+
+    #[test]
+    fn two_components_union() {
+        let mut uf = UnionFind::new(4);
+        uf.union(0, 1);
+        uf.union(2, 3);
+
+        assert_eq!(uf.connected(0, 1), true);
+        assert_eq!(uf.connected(2, 3), true);
+        assert_eq!(uf.connected(0, 3), false);
+
+        uf.union(1, 2);
+        assert_eq!(uf.count(), 1);
+        assert_eq!(uf.connected(0, 3), true);
+    }
+
+    #[test]
+    fn three_components() {
+        let mut uf = UnionFind::new(6);
+        uf.union(0, 1);
+        uf.union(2, 3);
+        uf.union(2, 4);
+
+        assert_eq!(uf.count(), 3);
+
+        uf.union(5, 0);
+        assert_eq!(uf.count(), 2);
+        assert_eq!(uf.connected(1, 5), true);
     }
 }
