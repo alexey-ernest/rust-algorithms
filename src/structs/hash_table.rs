4b2393650674672bbe5e952ffd54ebdc5bcec00f
Alexey <alexey.ernest@gmail.com>
2020-10-29 17:57:22 -0700

    Linear-probing hash table refactoring

--- src/structs/hash_table.rs
+++ src/structs/hash_table.rs
@@ -1,4 +1,4 @@
-/// Hash table auto-resizable linear-probing implementation with O(1) amortized complexity.
+/// Auto-resizable hash table linear-probing implementation with O(1) amortized cost.
 pub struct HashTable<T> {
     keys: Vec<Option<usize>>,
     data: Vec<Option<T>>,
@@ -12,8 +12,16 @@ const MIN_FILL_PERCENT: f32 = 1.0/8.0;
 impl<T> HashTable<T> where T: Copy {
     pub fn new() -> HashTable<T> {
         HashTable {
-            keys: vec![Option::None; INIT_SIZE],
-            data: vec![Option::None; INIT_SIZE],
+            keys: vec![None; INIT_SIZE],
+            data: vec![None; INIT_SIZE],
+            count: 0,
+        }
+    }
+
+    pub fn new_size(size: usize) -> HashTable<T> {
+        HashTable {
+            keys: vec![None; size],
+            data: vec![None; size],
             count: 0,
         }
     }
@@ -31,28 +39,32 @@ impl<T> HashTable<T> where T: Copy {
 
     fn rehash(&mut self, newsize: usize) {
         let mut h = HashTable {
-            keys: vec![Option::None; newsize],
-            data: vec![Option::None; newsize],
+            keys: vec![None; newsize],
+            data: vec![None; newsize],
             count: 0,
         };
         for (i, k) in self.keys.iter().enumerate() {
-            if !k.is_some() {
-                continue;
+            match k {
+                None => {
+                    continue;
+                }
+                Some(key) => {
+                    h.set(*key, self.data[i].unwrap());
+                }
             }
-            h.set(k.unwrap(), self.data[i].unwrap());
         }
         *self = h;
     }
 
     pub fn set(&mut self, key: usize, value: T) {
         let index = self.get_index(key);
-        self.keys[index] = Option::Some(key);
-        self.data[index] = Option::Some(value);
+        self.keys[index] = Some(key);
+        self.data[index] = Some(value);
         self.count += 1;
 
         let fill_percent = (self.count as f32)/(self.data.len() as f32);
         if fill_percent >= MAX_FILL_PERCENT {
-            self.rehash(2*self.data.len());
+            self.rehash(self.data.len()*2);
         }
     }
 
@@ -113,7 +125,7 @@ mod test {
     fn set_get_one() {
         let mut h = HashTable::new();
         h.set(1, 5);
-        assert_eq!(h.get(1), Option::Some(5));
+        assert_eq!(h.get(1), Some(5));
     }
 
     #[test]
@@ -121,8 +133,8 @@ mod test {
         let mut h = HashTable::new();
         h.set(1, 5);
         h.set(2, 3);
-        assert_eq!(h.get(1), Option::Some(5));
-        assert_eq!(h.get(2), Option::Some(3));
+        assert_eq!(h.get(1), Some(5));
+        assert_eq!(h.get(2), Some(3));
     }
 
     #[test]
@@ -131,15 +143,15 @@ mod test {
         h.set(1, 1);
         h.set(2, 2);
         h.set(5, 5);
-        assert_eq!(h.get(1), Option::Some(1));
-        assert_eq!(h.get(2), Option::Some(2));
-        assert_eq!(h.get(5), Option::Some(5));
+        assert_eq!(h.get(1), Some(1));
+        assert_eq!(h.get(2), Some(2));
+        assert_eq!(h.get(5), Some(5));
 
         h.delete(1);
 
-        assert_eq!(h.get(1), Option::None);
-        assert_eq!(h.get(2), Option::Some(2));
-        assert_eq!(h.get(5), Option::Some(5));
+        assert_eq!(h.get(1), None);
+        assert_eq!(h.get(2), Some(2));
+        assert_eq!(h.get(5), Some(5));
     }
 
     #[test]
@@ -148,15 +160,15 @@ mod test {
         h.set(1, 1);
         h.set(6, 6);
         h.set(11, 11);
-        assert_eq!(h.get(1), Option::Some(1));
-        assert_eq!(h.get(6), Option::Some(6));
-        assert_eq!(h.get(11), Option::Some(11));
+        assert_eq!(h.get(1), Some(1));
+        assert_eq!(h.get(6), Some(6));
+        assert_eq!(h.get(11), Some(11));
 
         h.delete(6);
 
-        assert_eq!(h.get(1), Option::Some(1));
-        assert_eq!(h.get(6), Option::None);
-        assert_eq!(h.get(11), Option::Some(11));
+        assert_eq!(h.get(1), Some(1));
+        assert_eq!(h.get(6), None);
+        assert_eq!(h.get(11), Some(11));
     }
 
     #[test]
@@ -170,7 +182,7 @@ mod test {
         assert_eq!(h.cap(), 15952);
 
         for i in 0..2000 {
-            assert_eq!(h.get(i), Option::Some(i));
+            assert_eq!(h.get(i), Some(i));
         }
 
         for i in 0..1000 {
@@ -181,7 +193,7 @@ mod test {
         assert_eq!(h.cap(), 7976);
 
         for i in 1000..2000 {
-            assert_eq!(h.get(i), Option::Some(i));
+            assert_eq!(h.get(i), Some(i));
         }
     }
 }
